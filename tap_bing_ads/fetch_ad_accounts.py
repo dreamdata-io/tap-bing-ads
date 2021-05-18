a1ef92bfe2354c8ccdd01bcb7f44591deda16be3
Patrick-Ranjit D. Madsen <pamad05@gmail.com>
2021-05-18 14:34:01 +0200

    remove dead code

--- tap_bing_ads/fetch_ad_accounts.py
+++ tap_bing_ads/fetch_ad_accounts.py
@@ -3,8 +3,6 @@ import logging
 import xmltodict
 
 from typing import Dict, Optional, Any, cast
-from collections import OrderedDict
-from requests.exceptions import HTTPError
 
 logger = logging.getLogger(__name__)
 
@@ -68,51 +66,3 @@ def request_customer_id(access_token: str, developer_token: str) -> int:
     )
 
     return int(cast(str, customer_id))
-
-
-def _request_ad_accounts(
-    customer_id: int, access_token: str, developer_token: str
-) -> list:
-    response = _request(
-        headers={"content-type": "text/xml", "SOAPAction": "GetAccountsInfo"},
-        data=f"""
-            <s:Envelope xmlns:i="http://www.w3.org/2001/XMLSchema-instance" xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
-                <s:Header xmlns="https://bingads.microsoft.com/Customer/v13">
-                    <Action mustUnderstand="1">GetAccountsInfo</Action>
-                    <AuthenticationToken i:nil="false">{access_token}</AuthenticationToken>
-                    <DeveloperToken i:nil="false">{developer_token}</DeveloperToken>
-                </s:Header>
-                <s:Body>
-                    <GetAccountsInfoRequest xmlns="https://bingads.microsoft.com/Customer/v13">
-                        <CustomerId i:nil="false">{customer_id}</CustomerId>
-                        <OnlyParentAccounts>false</OnlyParentAccounts>
-                    </GetAccountsInfoRequest>
-                </s:Body>
-            </s:Envelope>
-        """,
-    )
-
-    ad_account_or_accounts = get_field(
-        "s:Envelope",
-        "s:Body",
-        "GetAccountsInfoResponse",
-        "AccountsInfo",
-        "a:AccountInfo",
-        obj=response,
-        default=[],
-    )
-
-    if isinstance(ad_account_or_accounts, (OrderedDict, dict)):
-        return [ad_account_or_accounts]
-
-    return ad_account_or_accounts
-
-
-def fetch_ad_accounts(access_token: str, developer_token: str):
-    customer_id = _request_customer_id(access_token, developer_token)
-
-    ad_accounts = _request_ad_accounts(customer_id, access_token, developer_token)
-
-    result_ad_accounts = [ad_account["a:Id"] for ad_account in ad_accounts]
-
-    return customer_id, result_ad_accounts
